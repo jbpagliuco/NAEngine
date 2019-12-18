218b29f0a99f34aac5f0d2eb807406d1fd94dac6
jbpagliuco <jbpagliuco@gmail.com>
2019-12-17 22:48:38 -0600

    NGA-ify swap chain. Unused at the moment.

---
+++ NAEngine/Renderer/NGA/NGASwapChain.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "NGACommon.h"
+
+#include "Base/OS/OS.h"
+
+namespace na
+{
+
+	struct NGASwapChainDesc
+	{
+		Window mWindow = INVALID_WINDOW;
+		int mBufferCount = 2;
+	};
+
+	class NGASwapChain
+	{
+		NGA_GPU_CLASS(NGASwapChain);
+
+	public:
+		bool Construct(const NGASwapChainDesc &desc);
+		void Destruct();
+
+		bool IsConstructed()const;
+
+		void SetFullscreenState(bool fullscreen)const;
+		void Present()const;
+
+#if defined(NGA_D3D11)
+	private:
+		struct IDXGISwapChain *mSwapChain;
+#endif
+	};
+}
