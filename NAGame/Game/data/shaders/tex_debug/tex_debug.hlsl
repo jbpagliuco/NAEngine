f014eb7c357ea3355a87626186e0d4dfc4698177
jbpagliuco <jbpagliuco@gmail.com>
2020-04-22 21:25:29 -0500

    Update tex_debug shader. Allow shaders to not have a material buffer.

--- NAGame/Game/data/shaders/tex_debug/tex_debug.hlsl
+++ NAGame/Game/data/shaders/tex_debug/tex_debug.hlsl
@@ -1,4 +1,5 @@
 #include <shader_common_vs.hlsli>
+#include <shader_common_ps.hlsli>
 
 // Vertex/Pixel inputs
 struct VertexInput {
@@ -9,24 +10,23 @@ struct VertexInput {
 
 struct PixelInput {
 	float4 svpos : SV_POSITION;
-	float3 position : POSITION0;
-	float3 normal : NORMAL0;
 	float2 texCoord : TEXCOORD0;
 };
 
-PixelInput main(VertexInput input)
+// Vertex shader
+PixelInput vsMain(VertexInput input)
 {
 	float4 pos = float4(input.position, 1.0f);
 
 	PixelInput output;
-	
 	output.svpos = mul(mul(viewProj, world), pos);
-	output.position = mul(world, pos);
-	
-	float4 normal = float4(normalize(input.normal), 0.0f);
-	output.normal = mul(normal, worldInverseTranspose).xyz;
-	
 	output.texCoord = input.texCoord;
-	
+
 	return output;
+}
+
+// Pixel Shader
+float4 psMain(PixelInput input) : SV_TARGET
+{
+	return float4(input.texCoord.x, input.texCoord.y, 0.0f, 1.0f);
 }
